4a255120e9632e6c69de8e1810fe3cd6eb7bb22e
EladBlaier <xd.EBlaier@idf.il>
2026-06-03 15:16:10 +0300

    init

---
+++ src/features/alerts/components/alert-preview/AlertPreview.tsx
@@ -0,0 +1,11 @@
+import { Typography, Box, Stack } from "@mui/material";
+
+const AlertPreview = () => {
+    <Box onClick={() => {}}>
+        <Stack direction="row">
+            <Typography>אירוע אירוע</Typography>
+        </Stack>
+    </Box>
+};
+
+export default AlertPreview;
