0fc419637c18cbfd4be09dc00571f016e51099a1
Itai Yamin <itaiyamin2004@gmail.com>
2026-06-03 19:45:30 +0300

    feat: change top bar with functionallity

--- src/features/chief/ChiefApp.tsx
+++ src/features/chief/ChiefApp.tsx
@@ -1,7 +1,10 @@
 import { Box } from '@mui/material';
+import { useEffect, useRef } from 'react';
 import { makeStyles } from 'tss-react/mui';
+import { useDeviceLocation } from '../../core/store/atoms/locationAtom';
 import GenericMap from '../map/components/GenericMap';
-import { CoordinatePill } from './components/CoordinatePill';
+import { useMap } from '../map/MapProvider';
+import { TopBar } from './components/TopBar';
 
 const useStyles = makeStyles()((theme) => ({
   root: {
@@ -13,10 +16,38 @@ const useStyles = makeStyles()((theme) => ({
 
 export const ChiefApp: React.FC = () => {
   const { classes } = useStyles();
+  const { location } = useDeviceLocation();
+  const { goToLocation } = useMap();
+
+  const lastUpdateRef = useRef<number>(0);
+
+  // Throttled effect to track location movements and heading
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      const now = Date.now();
+      const timeSinceLastUpdate = now - lastUpdateRef.current;
+      let timeoutId: ReturnType<typeof setTimeout>;
+
+      const executeUpdate = () => {
+        goToLocation([location.longitude!, location.latitude!], 19.5, location.heading ?? undefined);
+        lastUpdateRef.current = Date.now();
+      };
+
+      if (timeSinceLastUpdate >= 1000) {
+        executeUpdate();
+      } else {
+        timeoutId = setTimeout(executeUpdate, 1000 - timeSinceLastUpdate);
+      }
+
+      return () => {
+        if (timeoutId) clearTimeout(timeoutId);
+      };
+    }
+  }, [location.latitude, location.longitude, location.heading, goToLocation]);
 
   return (
     <Box className={classes.root}>
-      <CoordinatePill />
+      <TopBar />
       <GenericMap />
     </Box>
   );
